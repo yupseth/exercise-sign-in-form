5a1fc9853b7b9fb71d17f39b3df74e6174eae5ea
yupseth <cristinayup@gmail.com>
2023-02-27 11:48:17 +0200

    initial selection

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const firstName = document.querySelector("#firstname");
+const lastName = document.querySelector("#lastname");
+const email = document.querySelector("#email");
+const phone = document.querySelector("#phone");
+const password = document.querySelector("#password");
+const passwordConfirm = document.querySelector("#password-confirm");
