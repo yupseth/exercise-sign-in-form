de2d30746202a7ec1aaca76aa42314641b2c5d66
yupseth <cristinayup@gmail.com>
2023-02-27 12:54:34 +0200

    test

--- script.js
+++ script.js
@@ -6,10 +6,3 @@ const email = document.querySelector("#email");
 const phone = document.querySelector("#phone");
 const password = document.querySelector("#password");
 const passwordConfirm = document.querySelector("#password-confirm");
-
-const firstNameError = document.querySelector(".firstname-error");
-const lastNameError = document.querySelector(".lastname-error");
-const emailError = document.querySelector(".email-error");
-const phoneError = document.querySelector(".phone-error");
-const passwordError = document.querySelector(".password-error");
-const passwordConfirmError = document.querySelector(".password-confirm-error");
